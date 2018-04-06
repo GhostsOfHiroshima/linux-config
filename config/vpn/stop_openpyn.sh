1496ffc7d0355ebaf83da88188c651679908a9d9
3dprogramin <admin@3dprogramin.com>
2018-04-06 23:07:32 +0300

    fix in filename

--- config/vpn/stop_openpyn.sh
+++ config/vpn/stop_openpyn.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 /usr/bin/openpyn --kill
-sleep 5
+sleep 2
 /usr/bin/openpyn -x            # reset firewall / kill flush
+sleep 2
+/home/icebox/Documents/scripts/public_ip.py        # update IP / location
+
